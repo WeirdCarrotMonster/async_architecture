84df74f684147e2b15739d4f206e5a7d7af712d2
Eugene Protozanov <weirdcarrotmonster@gmail.com>
2022-10-08 19:20:42 +0400

    Renamed backend → task_tracker

--- task_tracker/task_tracker/__main__.py
+++ task_tracker/task_tracker/__main__.py
@@ -2,7 +2,7 @@ import uvicorn
 
 
 def main():
-    uvicorn.run("backend.app:app", port=8000, host="0.0.0.0")
+    uvicorn.run("task_tracker.app:app", port=8000, host="0.0.0.0")
 
 
 if __name__ == "__main__":
