a26f101cb81e9c9d45486cf1f50eb539c53262e3
Eugene Protozanov <weirdcarrotmonster@gmail.com>
2022-10-08 23:17:16 +0400

    Added authentication view

--- auth_service/auth_service/views.py
+++ auth_service/auth_service/views.py
@@ -1,5 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from datetime import datetime, timedelta
 
+import jwt
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+
+from auth_service.config import settings
 from auth_service.domain import model
 from auth_service.service_layer.unit_of_work import AbstractUnitOfWork, get_unit_of_work
 from auth_service.service_layer import user as user_service
@@ -43,3 +48,25 @@ async def update_user(
         user = await user_service.update_user(uow, user_id, request)
 
     return user
+
+
+class AuthResponse(BaseModel):
+    token: str
+
+
+@router.post("/auth", response_model=AuthResponse)
+async def authenticate_user(
+    request: model.AuthenticateUserRequest,
+    uow: AbstractUnitOfWork = Depends(get_unit_of_work),
+):
+    async with uow:
+        user = await user_service.authenticate_user(uow, request)
+
+    if not user:
+        raise HTTPException(status_code=403)
+
+    expires = datetime.utcnow() + timedelta(hours=1)
+    token = jwt.encode(
+        {"public_id": user.public_id, "exp": expires}, settings.auth_secret
+    )
+    return AuthResponse(token=token)
